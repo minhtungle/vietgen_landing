090c4b90aac6e4b0b80766153a3d9b59d3dd593d
minhtungle <tunglm@huongvietgroup.com>
2024-06-08 01:58:19 +0700

    7

--- source/js/script.js
+++ source/js/script.js
@@ -170,7 +170,7 @@ class Main {
       slidesToScroll: 1,
       autoplay: true,
       dots: true,
-      speed: 600,
+      speed: 800,
       arrows: true,
       prevArrow: '<button type="button" class="carousel-control left" aria-label="carousel-control"><i class="fas fa-chevron-left"></i></button>',
       nextArrow: '<button type="button" class="carousel-control right" aria-label="carousel-control"><i class="fas fa-chevron-right"></i></button>'
@@ -186,7 +186,7 @@ class Main {
       slidesToScroll: 1,
       autoplay: true,
       dots: false,
-      speed: 600,
+      speed: 800,
       arrows: true,
       prevArrow: '<button type="button" class="carousel-control left" aria-label="carousel-control"><i class="fas fa-chevron-left"></i></button>',
       nextArrow: '<button type="button" class="carousel-control right" aria-label="carousel-control"><i class="fas fa-chevron-right"></i></button>'
@@ -201,7 +201,7 @@ class Main {
       slidesToScroll: 1,
       autoplay: true,
       dots: false,
-      speed: 600,
+      speed: 800,
       arrows: true,
       prevArrow: '<button type="button" class="carousel-control left" aria-label="carousel-control"><i class="fas fa-chevron-left"></i></button>',
       nextArrow: '<button type="button" class="carousel-control right" aria-label="carousel-control"><i class="fas fa-chevron-right"></i></button>'
--- theme/js/script.js
+++ theme/js/script.js
@@ -177,7 +177,7 @@ class Main {
       slidesToScroll: 1,
       autoplay: true,
       dots: true,
-      speed: 600,
+      speed: 800,
       arrows: true,
       prevArrow: '<button type="button" class="carousel-control left" aria-label="carousel-control"><i class="fas fa-chevron-left"></i></button>',
       nextArrow: '<button type="button" class="carousel-control right" aria-label="carousel-control"><i class="fas fa-chevron-right"></i></button>'
@@ -193,7 +193,7 @@ class Main {
       slidesToScroll: 1,
       autoplay: true,
       dots: false,
-      speed: 600,
+      speed: 800,
       arrows: true,
       prevArrow: '<button type="button" class="carousel-control left" aria-label="carousel-control"><i class="fas fa-chevron-left"></i></button>',
       nextArrow: '<button type="button" class="carousel-control right" aria-label="carousel-control"><i class="fas fa-chevron-right"></i></button>'
@@ -208,7 +208,7 @@ class Main {
       slidesToScroll: 1,
       autoplay: true,
       dots: false,
-      speed: 600,
+      speed: 800,
       arrows: true,
       prevArrow: '<button type="button" class="carousel-control left" aria-label="carousel-control"><i class="fas fa-chevron-left"></i></button>',
       nextArrow: '<button type="button" class="carousel-control right" aria-label="carousel-control"><i class="fas fa-chevron-right"></i></button>'
